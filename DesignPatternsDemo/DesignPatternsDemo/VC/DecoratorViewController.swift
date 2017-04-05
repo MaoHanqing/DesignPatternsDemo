4d6499c2cddebf9603f42dc21bee272f2640daa8
guokebing <243520104@qq.com>
2017-04-05 16:37:21 +0800

    no message

---
+++ DesignPatternsDemo/DesignPatternsDemo/VC/DecoratorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DecoratorViewController.swift
+//  DesignPatternsDemo
+//
+//  Created by Guo Kebing on 2017/4/5.
+//  Copyright © 2017年 MHQ. All rights reserved.
+//
+
+import UIKit
+
+class DecoratorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
